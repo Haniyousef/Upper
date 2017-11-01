cce0db71adb2fde93071499986abac362c07d0a6
haniyousef <haniyousef49@gmail.com>
2017-11-01 18:42:28 +0200

    Profile Image

--- app/src/main/java/com/example/hani/upper/CustomerSettingsActivity.java
+++ app/src/main/java/com/example/hani/upper/CustomerSettingsActivity.java
@@ -1,15 +1,24 @@
 package com.example.hani.upper;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -17,7 +26,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,7 +40,9 @@ public class CustomerSettingsActivity extends AppCompatActivity {
     Button confirm_btn , back_btn ;
     private FirebaseAuth mAuth ;
     private DatabaseReference mRef ;
-    private String userId , name , phone;
+    private String userId , name , phone , profileImageUrl;
+    private ImageView profile ;
+    private Uri resultUri ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +51,22 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         phoneFeild=(EditText)findViewById(R.id.phone_edit);
         confirm_btn=(Button)findViewById(R.id.confirm_btn);
         back_btn=(Button) findViewById(R.id.back_btn);
+        profile=(ImageView) findViewById(R.id.image_profile);
 
         mAuth=FirebaseAuth.getInstance();
         userId=mAuth.getCurrentUser().getUid();
         mRef= FirebaseDatabase.getInstance().getReference().child("Users").child("Customer").child(userId);
 
         getCustomerInformation();
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,1);
+            }
+        });
 
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +99,10 @@ public class CustomerSettingsActivity extends AppCompatActivity {
                        phone = map.get("phone").toString();
                        phoneFeild.setText(phone);
                    }
+                   if (map.get("imageProfile") != null){
+                       profileImageUrl = map.get("imageProfile").toString();
+                       Glide.with(CustomerSettingsActivity.this).load(profileImageUrl).into(profile);
+                   }
                }
            }
 
@@ -101,5 +131,53 @@ public class CustomerSettingsActivity extends AppCompatActivity {
             }
         });
 
+        // save image
+        if (resultUri!=null){
+            StorageReference storage=FirebaseStorage.getInstance().getReference().child("profile_images").child(userId);
+            Bitmap bitmap=null ;
+            try {
+                bitmap= MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(),resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            ByteArrayOutputStream baos=new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG,20,baos);
+            byte[] data =baos.toByteArray();
+            final UploadTask task=storage.putBytes(data);
+
+            task.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(CustomerSettingsActivity.this,"error upload image", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            task.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri img_url= taskSnapshot.getDownloadUrl();
+                    Map new_image=new HashMap();
+                    new_image.put("imageProfile",img_url.toString());
+                    mRef.updateChildren(new_image);
+                    finish();
+                    return;
+                }
+            });
+
+        }else{
+            Toast.makeText(this,"error upload image", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1 && resultCode== Activity.RESULT_OK){
+            final Uri imageUri=data.getData();
+            resultUri=imageUri ;
+            profile.setImageURI(resultUri);
+        }
     }
 }
